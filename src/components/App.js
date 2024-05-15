c6415a4f069759c0273e0bee28c59b818a0cc537
Maxwel <maxwellbakora@gmail.com>
2024-05-15 22:45:49 +0300

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,12 @@
 import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
-import ProjectList from "./ProjectList";
-import user from "../data/user";
+import ColorList from "./ColorList";
+import Objects from "./Objects";
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <ColorList />
+      <Objects/>
     </div>
   );
 }
